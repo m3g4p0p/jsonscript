cec41d79962d1302a00faa99310e8f6a0478fa1e
Sebastian Seitz <bassistsagainstracists@gmx.net>
2020-06-26 19:58:01 +0200

    add STD comments

--- src/jsonscript/std.py
+++ src/jsonscript/std.py
@@ -1,18 +1,24 @@
 STD = {
+    # Control flow
     'if': lambda condition, if_true, if_false=None: (
         if_true if condition else if_false
     ),
-    'print': lambda *params: print(' '.join(map(str, params))),
+    # Logical operators
     'or': lambda x, y: x or y,
     'and': lambda x, y: x and y,
     'not': lambda x: not x,
+    # Comparisons
     'is': lambda x, y: x == y,
+    # Arithmetics
     '+': lambda x, y: x + y,
     '-': lambda x, y: x - y,
     '*': lambda x, y: x * y,
     '/': lambda x, y: x / y,
-    'push': lambda list, value: list.append(value) or value,
+    # Lists
+    'push': lambda list, value: list.append(value),
     'pop': lambda list: list.pop(),
     'length': lambda list: len(list),
-    'copy': lambda list: list.copy()
+    'copy': lambda list: list.copy(),
+    # Miscellaneous
+    'print': lambda *params: print(' '.join(map(str, params))),
 }
